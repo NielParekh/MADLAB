5e472c0a3aa2d59a0a200b6dc0735c1dd3aebd27
Niel Parekh <55613531+NielParekh@users.noreply.github.com>
2021-10-29 18:35:17 +0530

    Add files via upload

---
+++ WebPage/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.webpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/page.html");
+
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                EditText url = (EditText) findViewById(R.id.url_text);
+                String url_text = url.getText().toString();
+                WebView webView = (WebView) findViewById(R.id.webView);
+                WebSettings webSettings = webView.getSettings();
+                webSettings.setJavaScriptEnabled(true);
+                webView.loadUrl(url_text);
+            }
+        });
+    }
+
+}
